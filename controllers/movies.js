45df8226bdc1694456babb11e22a931f49c71244
DariaP9315 <79569262+DariaP9315@users.noreply.github.com>
2021-10-29 11:32:51 +0400

    Update movies.js

--- controllers/movies.js
+++ controllers/movies.js
@@ -45,7 +45,7 @@ module.exports.createMovie = (req, res, next) => {
     .then((movie) => res.send(movie))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        throw new BadRequestError(errorMessages.incorrectDataMovieCreate);
+        throw new BadRequestError('Переданы некорректные данные при создании фильма.');
       }
       next(err);
     })
