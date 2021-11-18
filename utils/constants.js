656fe5c9fcedd971c54d797c15ae48538b8a5bdf
DariaP9315 <79569262+DariaP9315@users.noreply.github.com>
2021-11-18 10:12:25 +0400

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,14 @@
-const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/moviesdb' } = process.env;
+const { PORT = 3000, 
+       DB_URL = 'mongodb://localhost:27017/moviesdb',
+      DB_SETTINGS = {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+       },
+       NODE_ENV,
+       JWT_SECRET,
+      } = process.env;
 
 const corsMethods = ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'];
 const corsHeaders = ['Authorization', 'Content-Type'];
@@ -28,4 +38,7 @@ module.exports = {
   corsMethods,
   corsHeaders,
   errorMessages,
+  DB_SETTINGS,
+  NODE_ENV,
+  JWT_SECRET,
 };
